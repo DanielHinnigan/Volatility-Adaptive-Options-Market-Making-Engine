174151f7a7496e57fed4a939e1a592ab96efc888
Daniel Hinnigan <dannyman@outlook.dk>
2026-07-10 14:21:24 +0200

    ADded constants for R and Q

--- src/config.py
+++ src/config.py
@@ -1,9 +1,13 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
+    # Settings for data
     DATA_PROVIDER: str = "yfinance"
     STORAGE_BACKEND: str = "parquet"
 
+    # Market settings
+    R: float = 0.045    # Risk-free rate - May be made more advanced later to adjust to market data
+    Q: float = 0.012
 
 # Create a single, global instance of the settings
 # Should be imported when using this config file
